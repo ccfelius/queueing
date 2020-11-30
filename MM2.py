40de1a215552a588859c0797cae90f3c8cfc00aa
ccfelius <lottefelius@Hotmail.com>
2020-11-30 17:14:50 +0100

    minimalized things

--- MM2.py
+++ MM2.py
@@ -6,27 +6,6 @@ from queueing.probabilities import *
 import pandas as pd
 import scipy.stats as st
 
-def conf_int(mean, var, n, p=0.95):
-    pnew = (p+1)/2
-    zval = st.norm.ppf(pnew)
-    sigma = math.sqrt(var)
-    alambda = (zval*sigma)/math.sqrt(n)
-    min_lambda = mean - alambda
-    plus_lambda = mean + alambda
-    return f"Confidence interval: [{min_lambda:.4f} < X < {plus_lambda:.4f}] with p = {p}"
-
-### SETTINGS
-# RANDOM_SEED = 33
-# SERVERS = 2 # amount of servers (n or c)
-# Set RHO to a little bit smaller then 1; makes the simulation interesting
-RHO = 0.9
-MU = 20 # 1/mu is exponential service times
-# MU > LAMBDA, if  mu = 1 and c is 1 otherwise no queue.
-# 1/MU > 1/LAMBDA if c=2 or higher?
-# If mu = 2, avg is every 0.5 time step is the time costs of a service.
-# suppose lambda < 1
-SIM_TIME = 100 # simulation time in time units
-
 class Queue(object):
     """
     Create the initial object queue
@@ -52,7 +31,7 @@ def customer(env, name, qu):
 
     a = env.now
     # print(f'{name} arrives at the servicedesk at {a:.2f}')
-    arrivals += 1
+    # arrivals += 1
 
     with qu.server.request() as request:
         yield request
@@ -70,7 +49,7 @@ def customer(env, name, qu):
 
         yield env.process(qu.service(name))
         # print('%s leaves the servicedesk at %.2f.' % (name, env.now))
-        leavers += 1
+        # leavers += 1
 
 
 def setup(env, servers, servicetime, t_inter):
@@ -96,68 +75,52 @@ def setup(env, servers, servicetime, t_inter):
 # Setup and start the simulation
 print('QUEUE SIMULATION\n')
 
-# set the amount of simulations per server instellingen
-SIMULATIONS = 200
-print(f'Simulations: {SIMULATIONS}')
+# Set servers = 1
+# set the amount of simulations
+SIMULATIONS = 10
+column = ['RHO', 'SIM_TIME', 'AVG_WAIT']
+data_sims = []
+run_data = pd.DataFrame(columns=column)
+SERVERS = 2 # fixed
+# RHO = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+RHO = [0.9, 0.1]
+MU = 1
+simtimes = [5, 25, 100, 150]
+# simtimes[0] = 1
 
-for servers in [1, 2, 4]:
-    # Create dataframe to store important values to calculate statistics
-    cols = ['AVG_WAITING', 'AVG_ARRIVING', 'AVG_LEAVING']
-    data = pd.DataFrame(columns=cols)
+# Simulations
+for rho in RHO:
 
-    print(f'\nServers (c): {servers}')
-    SERVERS = servers
-    LAMBDA = RHO * (MU * SERVERS)  # 1/lambda is exponential inter arrival times
+    LAMBDA = rho * (MU * SERVERS)  # 1/lambda is exponential inter arrival times
 
-    print("EXPECTED VALUES AND PROBABILITIES")
+    for SIM_TIME in simtimes:
 
-    print(f'Rho: {RHO}\nMu: {MU}\nLambda: {LAMBDA}\nExpected interarrival time: {1 / LAMBDA:.2f} time units')
-    print(f'Expected processing time per server: {1 / MU:.2f} time units\n')
-    print(f'Probability that a job has to wait: {pwait(SERVERS, RHO):.2f}')
-    print(f'Expected waiting time E(W): {expw(MU, SERVERS, RHO):.2f} time units')
-    print(f'Expected queue length E(Lq): {expquel(SERVERS, RHO):.2f} customers\n')
-
-    # e.g. Lambda = 3 gives 1/3 per unit in simulation time, so avg is around every 0.33 timestep a new customer occurs
-    # 0.33 timestep could be 0.33 hour = 0.33*60 = 19.8 minutes (on average)
-
-    for s in range(SIMULATIONS):
-
-        waiting_time = 0
-        counter = 0
-        arrivals = 0
-        leavers = 0
-
-        # Create an environment and start the setup process
-        env = simpy.Environment()
-        env.process(setup(env, SERVERS, MU, LAMBDA))
-
-        # Execute the simulation
-        env.run(until=SIM_TIME)
-
-        rho = LAMBDA/(SERVERS*MU)
-        avg_waiting = waiting_time/(counter)
-        avg_arrivals = arrivals/SIM_TIME
-        avg_leavers = leavers/SIM_TIME
-
-        data.loc[s] = [avg_waiting, avg_arrivals, avg_leavers]
-
-        # print(f'Simulation {s+1}')
-        # print(f'Average waiting time: {avg_waiting:.3f} time units')
-        # print(f'Avg customers arriving per time unit: {avg_arrivals:.3f} time units')
-        # print(f'Avg customers leaving per time unit: {avg_leavers:.3f} time units\n')
-
-    # print dataframe with data
-    # print(data)
-    print(f'Expected waiting time E(W): {expw(MU, SERVERS, RHO):.3f} time units')
-    print(f'AVG waiting: {data["AVG_WAITING"].mean():.3f} time units')
-    print(conf_int(data["AVG_WAITING"].mean(), data["AVG_WAITING"].var(), SIMULATIONS, p=0.95))
-    print()
-    print(f'AVG arriving: {data["AVG_ARRIVING"].mean():.3f} per time unit')
-    print(conf_int(data["AVG_ARRIVING"].mean(), data["AVG_ARRIVING"].var(), SIMULATIONS, p=0.95))
-    print()
-    print(f'AVG leaving: {data["AVG_LEAVING"].mean():.3f} per time unit')
-    print(conf_int(data["AVG_LEAVING"].mean(), data["AVG_LEAVING"].var(), SIMULATIONS, p=0.95))
+        # Create dataframe to store important values to calculate statistics
+        data = pd.DataFrame(columns=column)
 
 
-# Make sure we need to variate rho?
-# also write to a file needs to be done
+        for s in range(SIMULATIONS):
+
+            waiting_time = 0
+            counter = 0
+            # arrivals = 0
+            # leavers = 0
+
+            # Create an environment and start the setup process
+            env = simpy.Environment()
+            env.process(setup(env, SERVERS, MU, LAMBDA))
+
+            # Execute the simulation
+            env.run(until=SIM_TIME)
+
+            avg_waiting = waiting_time/(counter)
+            # avg_arrivals = arrivals/SIM_TIME
+            # avg_leavers = leavers/SIM_TIME
+
+            data.loc[s] = [rho, SIM_TIME, avg_waiting]
+
+        data_sims.append(data)
+
+print(pd.concat(data_sims))
+
+pd.concat(data_sims).to_csv('test.txt', sep='\t', index=False)
